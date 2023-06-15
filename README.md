73d692332d3192ed5229ad045f831c376466f06c
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-06-14 21:25:13 -0400

    working version

---
+++ README.md
@@ -0,0 +1,3 @@
+This mod prevents the OpenGL `GL_INVALID_ENUM` error from being logged more than once. If you know what that is, this is probably for you. If not, don't worry about it. This is a tool for people who read the debug log (mainly developers).
+
+Click [here](https://github.com/AdamRaichu/suppress-OpenGL-1280/wiki) for more information on what that error is.
--- src/main/java/io/github/adamraichu/suppressopengl1280/mixin/GlDebugMixin.java
+++ src/main/java/io/github/adamraichu/suppressopengl1280/mixin/GlDebugMixin.java
@@ -1,17 +1,34 @@
 package io.github.adamraichu.suppressopengl1280.mixin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.client.gl.GlDebug;
 
 @Mixin(GlDebug.class)
 public abstract class GlDebugMixin {
-  boolean hasPostedMessage = false;
+  private static boolean hasPostedMessage = false;
 
-  @Inject(at = @At(value = "HEAD"), method = "info(IIIIIjj)V")
-  private void suppressMessage(int source, int type, int id, int severity, int messageLength, long message, long l, CallbackInfo ci) {
-    if () {}
-  } 
+  private static Logger LOGGER = LoggerFactory.getLogger("Suppress OpenGL Error 1280");
+
+  @Inject(at = @At(value = "HEAD"), method = "info(IIIIIJJ)V", cancellable = true)
+  private static void suppressMessage(int source, int type, int id, int severity, int messageLength, long message,
+      long l,
+      CallbackInfo ci) {
+    if (!(id == 1280)) {
+      return;
+    }
+    if (hasPostedMessage) {
+      ci.cancel();
+    } else {
+      LOGGER.info("OpenGL error 1280 (GL_INVALID_ENUM) is known to spam several times per second.");
+      LOGGER.info("This is the last time (for this run) that an error with this id will be logged.");
+      LOGGER.info("Go to https://github.com/AdamRaichu/suppress-opengl-1280/wiki for more info about this error.");
+      hasPostedMessage = true;
+    }
+  }
 }
