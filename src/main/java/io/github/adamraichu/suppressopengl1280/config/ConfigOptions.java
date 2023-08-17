a2b1d004b83e51475babce081c40f1904e145256
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-08-17 13:54:45 -0400

    add lang file
    add tooltip to `suppress1280` config option

--- src/main/java/io/github/adamraichu/suppressopengl1280/config/ConfigOptions.java
+++ src/main/java/io/github/adamraichu/suppressopengl1280/config/ConfigOptions.java
@@ -2,10 +2,16 @@ package io.github.adamraichu.suppressopengl1280.config;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "suppressopengl1280")
 public class ConfigOptions implements ConfigData {
+  @ConfigEntry.Gui.Tooltip()
   public boolean suppress1280 = true;
+
+  // @ConfigEntry.Gui.Tooltip()
   public boolean suppress1281 = true;
+
+  // @ConfigEntry.Gui.Tooltip()
   public boolean suppress1282 = true;
 }
